78b42b39ea8007ae18e37343e309cf38fc1fe052
Erick <ariel99.en@gmail.com>
2020-10-01 00:15:27 -0500

    Creacion de las views

--- peliculas/appmovie/views.py
+++ peliculas/appmovie/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.views.generic.base import TemplateView
 # Create your views here.
+
+class InicioView(TemplateView):
+    template_name = "index.html"
+
+
+# vistas de Peliculas
+class PeliculaView(TemplateView):
+     
+    template_name = "pelicula.html"
+     
